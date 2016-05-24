94be2939d946aa1285c426af7d64a28272923c97
CAH-josephmcenery <joseph.mcenery@cardinalhealth.com>
2016-05-24 11:39:50 -0400

    Sending data to firebase database
    This commit implements the switch from an intent to the email app to
    sending the data directly to a firebase database.

--- app/src/main/java/net/nerdhelp/nerdhelpalpha/RequestActivity.java
+++ app/src/main/java/net/nerdhelp/nerdhelpalpha/RequestActivity.java
@@ -1,48 +1,64 @@
 package net.nerdhelp.nerdhelpalpha;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.provider.Settings.Secure;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RequestActivity extends AppCompatActivity {
     String[] joeysEmail = {"joey.mcenery@gmail.com"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
     }
-public void onSubmit(View view){
-    EditText nameF = (EditText) findViewById(R.id.name);
-    String name = nameF.getText().toString();
 
-    EditText emailAddressF = (EditText) findViewById(R.id.email_address);
-    String emailAddress = emailAddressF.getText().toString();
+    public void onSubmit(View view) {
+        EditText nameF = (EditText) findViewById(R.id.name);
+        String name = nameF.getText().toString();
 
-    EditText phoneNumberF = (EditText) findViewById(R.id.phone_number);
-    String phoneNumber = phoneNumberF.getText().toString();
+        EditText emailAddressF = (EditText) findViewById(R.id.email_address);
+        String emailAddress = emailAddressF.getText().toString();
 
-    EditText streetAddressF = (EditText) findViewById(R.id.street_address);
-    String streetAddress = streetAddressF.getText().toString();
+        EditText phoneNumberF = (EditText) findViewById(R.id.phone_number);
+        String phoneNumber = phoneNumberF.getText().toString();
 
-    EditText problemDescriptionF = (EditText) findViewById(R.id.problem_description);
-    String problemDescription = problemDescriptionF.getText().toString();
+        EditText streetAddressF = (EditText) findViewById(R.id.street_address);
+        String streetAddress = streetAddressF.getText().toString();
 
-    String emailBody = (""+ name+"\n"+emailAddress+"\n"+phoneNumber+"\n"+streetAddress+"\n"+problemDescription);
-    createEmail(joeysEmail, "NerdHelp request via android app", emailBody);
-}
-    public void createEmail(String[] addresses, String subject, String body){
+        EditText problemDescriptionF = (EditText) findViewById(R.id.problem_description);
+        String problemDescription = problemDescriptionF.getText().toString();
 
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        intent.putExtra(Intent.EXTRA_TEXT, body);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
+        writeToFirebase(name, emailAddress, phoneNumber, streetAddress, problemDescription);
+    }
+
+    public void writeToFirebase(String name, String email, String phone, String add, String prbdesc) {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("inquiries");
+        myRef.child("newInquiryFrom  " + name).child("name").setValue(name);
+        myRef.child("newInquiryFrom  " + name).child("email").setValue(email);
+        myRef.child("newInquiryFrom  " + name).child("phone").setValue(phone);
+        myRef.child("newInquiryFrom  " + name).child("address").setValue(add);
+        myRef.child("newInquiryFrom  " + name).child("problem desc").setValue(prbdesc);
+
+        //display submittal message
+        Context context = getApplicationContext();
+        CharSequence text = "Your inquiry has been submitted! NerdHelp will be contacting you soon!";
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        Intent intent = new Intent(RequestActivity.this, NaviActivity.class);
+        startActivity(intent);
     }
 }
